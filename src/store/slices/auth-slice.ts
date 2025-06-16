9b77090314092a6b5371c4c151b9bd115ab996c5
ANKIT3058 <arunkr266@gmail.com>
2025-06-16 23:02:15 +0530

    Client logout

--- src/store/slices/auth-slice.ts
+++ src/store/slices/auth-slice.ts
@@ -2,11 +2,15 @@ import { UserType } from "@/types/user";
 import { StateCreator } from "zustand";
 
 export interface AuthSlice {
-    userInfo: undefined | UserType;
-    setUserInfo: (userInfo: UserType) => void;
+  userInfo: undefined | UserType;
+  setUserInfo: (userInfo: UserType) => void;
+  logOut: () => void;
 }
 
-export const createAuthSlice : StateCreator<AuthSlice> = (set) => ({
-    userInfo: undefined,
-    setUserInfo: (userInfo: UserType) => set({userInfo}),
-})
+export const createAuthSlice: StateCreator<AuthSlice> = (set) => ({
+  userInfo: undefined,
+  setUserInfo: (userInfo: UserType) => set({ userInfo }),
+  logOut: () => {
+    set({ userInfo: undefined });
+  },
+});
